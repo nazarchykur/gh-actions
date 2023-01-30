7168611c1e9098ea5ae5f883d126cbcb776c8bff
NazarChykur <nazar.chykur@emburse.com>
2023-01-30 17:44:50 +0200

    init

---
+++ src/main/java/com/example/ghactions/GhActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhActionsApplication.class, args);
+    }
+
+}
